22567e1504e3e309d3ea97be05d11f18e1cad5db
Israel-machine <israel.diaz1701@gmail.com>
2026-03-14 13:11:19 -0700

    show toys

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ToyCard from "./ToyCard";
 
 function ToyContainer() {
+  const [toys, setToys] = useState([])
+  const fetchToys = () => {
+    fetch('http://localhost:3000/toys')
+      .then(response => response.json())
+      .then(toys => setToys(toys))
+      .catch((e) => console.log(e))
+  }
+  useEffect(() => {
+    fetchToys()
+  }, [])
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">
+      {toys.map((toy) => <ToyCard key={toy.id} name={toy.name} image={toy.image} likes={toy.likes} />)}
+    </div>
   );
 }
 
