2b9250b83efce4f14ad0d76eb6a46c343fb4b88b
Israel-machine <israel.diaz1701@gmail.com>
2026-03-14 13:34:58 -0700

    donate toys

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function ToyCard({ name, image, likes }) {
+function ToyCard({ name, image, likes, toyId, donateToy }) {
   return (
     <div className="card" data-testid="toy-card">
       <h2>{name}</h2>
@@ -11,7 +11,7 @@ function ToyCard({ name, image, likes }) {
       />
       <p>{likes} Likes </p>
       <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      <button className="del-btn" onClick={() => donateToy(toyId)}>Donate to GoodWill</button>
     </div>
   );
 }
--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,10 +1,26 @@
 import React, { useEffect, useState } from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer({ toys }) {
+function ToyContainer({ toys, setToys }) {
+  const donateToy = (toyId) => {
+    fetch(`http://localhost:3000/toys/${toyId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+      .then(response => response.json())
+      .then(donatedToy => {
+        const updatedToys = toys.filter(toy => toy.id !== donatedToy.id);
+        setToys(updatedToys)
+      })
+      .catch((e) => console.log(e))
+  }
   return (
     <div id="toy-collection">
-      {toys.map((toy) => <ToyCard key={toy.id} name={toy.name} image={toy.image} likes={toy.likes} />)}
+      {toys.map((toy) =>
+        <ToyCard key={toy.id} name={toy.name} image={toy.image} likes={toy.likes} toyId={toy.id} donateToy={donateToy} />
+      )}
     </div>
   );
 }
